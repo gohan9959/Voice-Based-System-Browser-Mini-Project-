f1fa37d0c3c0605fbc6bc56cd4e0307ea0222113
Harhon <hsgohon1@gmail.com>
2018-05-18 13:11:11 +0530

    few changes to openFolder.py code

--- folderOpen.py
+++ folderOpen.py
@@ -4,17 +4,12 @@ def op(request):
     path=""
     y="f"
     x=y+":"
-    if(request.lower()!="exit"):
-        for root, dirs, files in os.walk(x,topdown=True):
-           for name in dirs:
-                a=os.path.join(root, name)
-                print(a)
-                s=a.split(":" or "\\")
-                if(request.lower()==s[len(s)-1].lower()):
-                    path=a
-                    os.startfile(path)
-                    sys.exit(0)
-            
-    else:
-        sys.exit(0)
-
+    for root, dirs, files in os.walk(x,topdown=True):
+        for name in dirs:
+            path=os.path.join(root, name)
+            print(path)
+            s1=path.split("\\")
+            s2=path.split(":")
+            if(request.lower()==s1[len(s1)-1].lower() or request.lower()==s2[len(s2)-1].lower()):
+                os.startfile(path)
+                sys.exit(0)
